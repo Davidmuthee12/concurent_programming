c4beeea4a6d38249cff71d24a1754d468d9f2faf
unknown <mutheedavid12@gmail.com>
2026-08-13 19:06:17 +0300

    feat:learn concurrency in golang

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Davidmuthee12/threads
+
+go 1.25.0
---
+++ threads/sequential.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func doWork(id int) {
+	fmt.Printf("work %d started at %s\n",id, time.Now().Format("15:04:05"))
+	time.Sleep(1 * time.Second)
+	fmt.Printf("work %d finisshed at %s\n", id, time.Now().Format("15:04:05"))
+}
+
+func main() {
+	for i := 0; i < 5; i++ {
+		go doWork(i)
+	}
+}
