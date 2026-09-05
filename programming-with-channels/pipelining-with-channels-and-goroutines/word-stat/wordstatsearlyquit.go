f46cec7af9af351b029d3ab630ab396644d4e75f
unknown <mutheedavid12@gmail.com>
2026-09-05 14:31:46 +0300

    feat: closing channels after a condition e.t.c

---
+++ programming-with-channels/pipelining-with-channels-and-goroutines/word-stat/wordstatsearlyquit.go
@@ -0,0 +1,233 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+	"sort"
+	"strings"
+	"sync"
+)
+
+func fanIn[K any](quit <-chan int, allChannels ...<-chan K) chan K {
+    wg := sync.WaitGroup{}
+    wg.Add(len(allChannels))
+    output := make(chan K)
+    for _, c := range allChannels {
+        go func(channel <-chan K) {
+            defer wg.Done()
+            for i := range channel {
+                select {
+                case output <- i:
+                case <-quit:
+                    return
+                }
+            }
+        }(c)
+    }
+    go func() {
+        wg.Wait()
+        close(output)
+    }()
+    return output
+}
+
+func take[K any](quit chan int, n int, input <-chan K) <-chan K {
+    output := make(chan K)
+    go func() {
+        defer close(output)
+        moreData := true
+        var msg K
+        for n > 0 && moreData {
+            select {
+            case msg, moreData = <-input:
+                if moreData {
+                    output <- msg
+                    n--
+                }
+            case <-quit:
+                return
+            }
+        }
+        if n == 0 {
+            close(quit)
+        }
+    }()
+    return output
+}
+
+func broadcast[K any](quit <-chan int, input <-chan K, n int) []chan K {
+    outputs := CreateAll[K](n)
+    go func() {
+        defer CloseAll(outputs...)
+        var msg K
+        moreData := true
+        for moreData {
+            select {
+            case msg, moreData = <-input:
+                if moreData {
+                    for _, output := range outputs {
+                        output <- msg
+                    }
+                }
+            case <-quit:
+                return
+            }
+        }
+    }()
+    return outputs
+}
+
+func CreateAll[K any](n int) []chan K {
+    channels := make([]chan K, n)
+    for i, _ := range channels {
+        channels[i] = make(chan K)
+    }
+    return channels
+}
+
+func CloseAll[K any](channels ...chan K) {
+    for _, output := range channels {
+        close(output)
+    }
+}
+
+
+func frequentWords(quit <-chan int, words <-chan string) <-chan string {
+    mostFrequentWords := make(chan string)
+    go func() {
+        defer close(mostFrequentWords)
+        frequencyMap := make(map[string]int)
+        frequencyList := make([]string, 0)
+        moreData, word := true, ""
+        for moreData {
+            select {
+            case word, moreData = <-words:
+                if moreData {
+                    if frequencyMap[word] == 0 {
+                        frequencyList = append(frequencyList, word)
+                    }
+                    frequencyMap[word] += 1
+                }
+            case <-quit:
+                return
+            }
+        }
+        sort.Slice(frequencyList, func(a, b int) bool {
+            return frequencyMap[frequencyList[a]] > frequencyMap[frequencyList[b]]
+        })
+        mostFrequentWords <- strings.Join(frequencyList[:10], ", ")
+    }()
+    return mostFrequentWords
+}
+
+func longestWords(quit <-chan int, words <-chan string) <-chan string {
+    longWords := make(chan string)
+    go func() {
+        defer close(longWords)
+        uniqueWordsMap := make(map[string]bool)
+        uniqueWords := make([]string, 0)
+        moreData, word := true, ""
+        for moreData {
+            select {
+            case word, moreData = <-words:
+                if moreData && !uniqueWordsMap[word] {
+                    uniqueWordsMap[word] = true
+                    uniqueWords = append(uniqueWords, word)
+                }
+            case <-quit:
+                return
+            }
+        }
+        sort.Slice(uniqueWords, func(a, b int) bool {
+            return len(uniqueWords[a]) > len(uniqueWords[b])
+        })
+        longWords <- strings.Join(uniqueWords[:10], ", ")
+    }()
+    return longWords
+}
+
+func extractWords(quit <-chan int, pages <-chan string) <-chan string {
+    words := make(chan string)
+    go func() {
+        defer close(words)
+        wordRegex := regexp.MustCompile(`[a-zA-Z]+`)
+        moreData, page := true, ""
+        for moreData {
+            select {
+            case page, moreData = <-pages:
+                if moreData {
+                    for _, word := range wordRegex.FindAllString(page, -1) {
+                        words <- strings.ToLower(word)
+                    }
+                }
+            case <-quit:
+                return
+            }
+        }
+    }()
+    return words
+}
+
+func downloadPages(quit <-chan int, urls <-chan string) <-chan string {
+    pages := make(chan string)
+    go func() {
+        defer close(pages)
+        moreData, url := true, ""
+        for moreData {
+            select {
+            case url, moreData = <-urls:
+                if moreData {
+                    resp, _ := http.Get(url)
+                    if resp.StatusCode != 200 {
+                        panic("Server's error: " + resp.Status)
+                    }
+                    body, _ := io.ReadAll(resp.Body)
+                    pages <- string(body)
+                    resp.Body.Close()
+                }
+            case <-quit:
+                return
+            }
+        }
+    }()
+    return pages
+}
+
+func generateUrls(quit <-chan int) <-chan string {
+    urls := make(chan string)
+    go func() {
+        defer close(urls)
+        for i := 100; i <= 130; i++ {
+            url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+            select {
+            case urls <- url:
+            case <-quit:
+                return
+            }
+        }
+    }()
+    return urls
+}
+
+const downloaders = 20
+
+func main() {
+    quitWords := make(chan int)
+    quit := make(chan int)
+    defer close(quit)
+    urls := generateUrls(quitWords)
+    pages := make([]<-chan string, downloaders)
+    for i := 0; i < downloaders; i++ {
+        pages[i] = downloadPages(quitWords, urls)
+    }
+    words := take(quitWords, 10000,
+        extractWords(quitWords, fanIn(quitWords, pages...)))
+    wordsMulti := broadcast(quit, words, 2)
+    longestResults := longestWords(quit, wordsMulti[0])
+    frequentResults := frequentWords(quit, wordsMulti[1])
+
+    fmt.Println("Longest Words:", <-longestResults)
+    fmt.Println("Most frequent Words:", <-frequentResults)
+}
