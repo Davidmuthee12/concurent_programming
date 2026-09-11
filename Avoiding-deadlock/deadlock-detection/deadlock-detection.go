5c937f4143936b8f0fbd58ab6f6eeae416668911
unknown <mutheedavid12@gmail.com>
2026-09-11 14:00:57 +0300

    feat:going around go's deadlock detection

--- Avoiding-deadlock/deadlock-detection/deadlock-detection.go
+++ Avoiding-deadlock/deadlock-detection/deadlock-detection.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func lockBoth(lock1, lock2 *sync.Mutex, wg *sync.WaitGroup) {
@@ -22,7 +23,13 @@ func main() {
 	// Starts two goroutines, locking both mutexes at the same time
 	go lockBoth(&lockA, &lockB, &wg)
 	go lockBoth(&lockB, &lockA, &wg)
-	// Waits for the goroutine to terminate
-	wg.Wait()
+	// Creates a goroutine that waits on the waitgroup before outputting a message
+	go func() {
+		wg.Wait()
+		fmt.Println("Done waiting on waitgroup")
+	}()
+	// waits for 30 seconds
+	time.Sleep(30 * time.Second)
+	// Outputs a message, and then the program terminates
 	fmt.Println("Done")
 }
